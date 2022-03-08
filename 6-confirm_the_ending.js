a2eca559a1e75eb5c710539a1897ffb6822aa57b
DarwinDemian <darwindemian@hotmail.com>
2022-03-08 13:09:12 -0300

    Added new algo

---
+++ 6-confirm_the_ending.js
@@ -0,0 +1,31 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/confirm-the-ending
+
+// O(n)
+// first solution
+function confirmEnding(str, target) {  
+  let newStr = [...str]
+  let strr = newStr.splice(str.length - target.length, target.length)
+
+  for (let i in strr) {
+    if (strr[i] !== target[i]) {
+      return false
+    } 
+  }
+
+  return true
+}
+
+// O(n)
+// second solution 
+const confirmEnding2 = (str, target) => {
+  for (let i = str.length - target.length; i < str.length; i++) {
+    if (str[i] !== target[target.indexOf(str[i])]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(confirmEnding("Bastian", "ian"), confirmEnding("Bastian", "bas"));
+console.log(confirmEnding2("Bastian", "ian"), confirmEnding2("Bastian", "bas"));
