e84c5171f62f9c8e1723fc061b162fc279d66c5f
DarwinDemian <darwindemian@hotmail.com>
2022-03-11 12:51:21 -0300

    Added algo

---
+++ 8-Truncate_a_string.js
@@ -0,0 +1,20 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/truncate-a-string
+
+// Truncate a string (first argument) if it is longer than the given 
+// maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+// O(1)
+function truncateString(str, num) {
+  if (num >= str.length) {
+    return str
+  }
+  
+  let string = [...str]
+  let newString = string.splice(0, num).join('') + "..."
+  
+  return newString;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)
